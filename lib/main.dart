97b7193661a9c449d1d590238cf6e3469de05bca
Lukaslakas31 <lukejuniel@gmail.com>
2023-05-02 16:02:40 +0800

    debug show checked mode banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,3 @@
-// Copyright 2013 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:lmsflutter/course.dart';
@@ -9,14 +5,6 @@ import 'package:lmsflutter/course_list.dart';
 import 'package:lmsflutter/dashboard.dart';
 import 'package:lmsflutter/login.dart';
 
-/// This sample app shows an app with two screens.
-///
-/// The first route '/' is mapped to [HomeScreen], and the second route
-/// '/details' is mapped to [DetailsScreen].
-///
-/// The buttons use context.go() to navigate to each destination. On mobile
-/// devices, each destination is deep-linkable and on the web, can be navigated
-/// to using the address bar.
 void main() => runApp(const MyApp());
 
 /// The route configuration.
