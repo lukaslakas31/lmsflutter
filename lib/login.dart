c659e09918716b4f9184b4e972f1b835e8ca9983
Lukaslakas31 <lukejuniel@gmail.com>
2023-05-02 16:01:37 +0800

    change to stateful widget

--- lib/login.dart
+++ lib/login.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
-
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   /// Constructs a [LoginScreen]
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
