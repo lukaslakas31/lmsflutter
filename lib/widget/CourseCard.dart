b1010bd5dea7bb08e3ea5c8376d7abe5eea730a2
Lukaslakas31 <lukejuniel@gmail.com>
2023-05-04 22:46:47 +0800

    fixed card

--- lib/widget/CourseCard.dart
+++ lib/widget/CourseCard.dart
@@ -28,70 +28,80 @@ class CourseCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Image(image: NetworkImage(course.source), fit: BoxFit.cover),
-          ListTile(
-            key: key,
-            title: Text(
-              course.code,
-              style: const TextStyle(
-                fontSize: 20,
-                fontFamily: 'Lexend',
-              ),
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child:
+                  Image(image: NetworkImage(course.source), fit: BoxFit.cover),
             ),
-            subtitle: Text(
-              course.name,
-              style: const TextStyle(
-                fontSize: 20,
-                fontFamily: 'Lexend',
+            ListTile(
+              key: key,
+              title: Text(
+                course.code,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Lexend',
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Text(
-              course.description,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 15,
-                fontFamily: 'Lexend',
-              ),
-            ),
-          ),
-          TextButton(
-            onPressed: () {
-              if (buttonText == 'Enroll Course') {
-                addCode!(course.code);
-                context.go('/course');
-              } else {
-                context.go('/course_detail/${course.code}');
-              }
-            },
-            style: TextButton.styleFrom(
-              backgroundColor: const Color.fromRGBO(251, 142, 55, 1),
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                side: const BorderSide(
-                  color: Color.fromRGBO(251, 142, 55, 1),
-                  width: 2,
+              subtitle: Text(
+                course.name,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Lexend',
                 ),
               ),
             ),
-            child: Text(
-              buttonText,
-              style: const TextStyle(
-                fontFamily: 'Lexend',
-                color: Colors.white,
-                fontSize: 18,
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                course.description,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 15,
+                  fontFamily: 'Lexend',
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 20)
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              child: TextButton(
+                onPressed: () {
+                  if (buttonText == 'Enroll Course') {
+                    addCode!(course.code);
+                    context.go('/course');
+                  } else {
+                    context.go('/course_detail/${course.code}');
+                  }
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(251, 142, 55, 1),
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    side: const BorderSide(
+                      color: Color.fromRGBO(251, 142, 55, 1),
+                      width: 2,
+                    ),
+                  ),
+                ),
+                child: Text(
+                  buttonText,
+                  style: const TextStyle(
+                    fontFamily: 'Lexend',
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
